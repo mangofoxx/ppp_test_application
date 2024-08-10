f207afbb1d947f7a330af6c2720883c8507e9ad0
mangofoxx <tijanacukic076@gmail.com>
2024-08-10 17:13:28 +0200

    feat: add home view function

--- test_application/test_ops/views.py
+++ test_application/test_ops/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+"""
+TODO Projects should be loaded into view
+"""
+
+
+class ProjectListView(ListView):
+    pass
+
+
+projects = [
+    {
+        'project_name': 'Project 1',
+        'project_description': 'Desc Project 1',
+        'date_added': 'Avgust 10 2024'
+    },
+    {
+        'project_name': 'Project 2',
+        'project_description': 'Desc Project 2',
+        'date_added': 'Avgust 10 2024'
+    }
+]
+
+
+def home(request):
+    context = {
+        'projects': projects
+    }
+    return render(request, 'test_ops/home.html', context=context)
