350e6d8d9cb3f836ebf9e70a2b7fb1a6a855d776
mangofoxx <tijanacukic076@gmail.com>
2024-08-11 13:21:48 +0200

    fix: Login and redirect user

--- test_application/test_application/urls.py
+++ test_application/test_application/urls.py
@@ -16,13 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as login_views
 
 from users import views as user_view
+from views import PatchLogoutView, UserLoginView
+from views import UserProfile
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('test_ops.urls')),
-    path('login/', login_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('register/', user_view.RegisterView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', PatchLogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('profile/', UserProfile.as_view(), name='profile'),
 ]
+
--- test_application/users/views.py
+++ test_application/users/views.py
@@ -1,8 +1,13 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LogoutView, LoginView
 from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
+from django.views import View
 from django.views.generic import CreateView
+from django.contrib import messages
 
-from users.forms import UserRegisterForm
+from users.forms import UserRegisterForm, UpdateUserProfileForm
 
 
 class RegisterView(SuccessMessageMixin, CreateView):
@@ -15,3 +20,45 @@ class RegisterView(SuccessMessageMixin, CreateView):
         response = super().form_valid(form)
         self.success_message = self.success_message % {'username': form.cleaned_data.get('username')}
         return response
+
+
+class UserLoginView(LoginView):
+    template_name = 'users/login.html'
+    redirect_authenticated_user = True
+    next_page = 'profile'
+
+
+class PatchLogoutView(LogoutView):
+    http_method_names = ["get", "post", "options"]
+
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+
+
+class UserProfile(LoginRequiredMixin, View):
+    redirect_field_name = ''
+    login_url = 'login'
+    template_name = 'users/profile.html'
+
+    def get(self, request):
+        user_form = UpdateUserProfileForm(instance=request.user)
+        return self.render_form(request, user_form=user_form)
+
+    def post(self, request):
+        user_form = UpdateUserProfileForm(request.POST, instance=request.user)
+
+        if user_form.is_valid():
+            user_form.save()
+            username = user_form.cleaned_data.get('username')
+            messages.success(request, f'Account updated for {username}')
+            return redirect('profile')
+
+        return self.render_form(request, user_form=user_form)
+
+    @staticmethod
+    def render_form(request, **kwargs):
+        context = {}
+        for key, value in kwargs.items():
+            context.update({key: value})
+
+        return render(request, 'users/profile.html', context=context)
