d38e6108eb237b42d9ae81e9e200c6a18c4f3a61
mangofoxx <tijanacukic076@gmail.com>
2024-08-13 20:40:05 +0200

    feat: Add test scenario create/update/view

--- test_application/test_ops/views.py
+++ test_application/test_ops/views.py
@@ -1,29 +1,13 @@
-from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DeleteView
 from django.views.generic.detail import DetailView
 
 from test_ops.models import Project, TestScenario
 
-"""
-TODO Projects should be loaded into view
-"""
-
 
 class ProjectListView(ListView):
     pass
-
-
-projects = [
-    {
-        'project_name': 'Project 1',
-        'project_description': 'Desc Project 1',
-        'date_added': 'Avgust 10 2024'
-    },
-    {
-        'project_name': 'Project 2',
-        'project_description': 'Desc Project 2',
-        'date_added': 'Avgust 10 2024'
-    }
-]
 
 
 class HomeView(ListView):
@@ -48,3 +32,69 @@ class TestScenarioView(DetailView):
     model = TestScenario
     template_name = 'test_ops/test_scenario_detail.html'
     context_object_name = 'tests'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['project'] = Project.objects.filter(id=self.object.project_id).first()
+        return context
+
+
+class TestScenarioCreateView(LoginRequiredMixin, CreateView):
+    model = TestScenario
+    fields = ['title', 'description']
+
+    def get_success_url(self):
+        return reverse_lazy('project-details', kwargs={'pk': self.object.project_id})
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        form.instance.project_id = self.kwargs.get('project_id')
+        return super().form_valid(form)
+
+
+class ProjectCreateView(LoginRequiredMixin, CreateView):
+    model = Project
+    fields = ['title', 'description']
+
+    def get_success_url(self):
+        return reverse_lazy('project-details', kwargs={'pk': self.object.pk})
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class ProjectDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Project
+    success_url = '/'
+
+    def test_func(self):
+        """
+        Check if current user is owner of the project
+        :return: Boolean
+        """
+        project = self.get_object()
+
+        if project.owner == self.request.user:  # Check if current user is owner
+            return True
+
+        return False
+
+
+class TestScenarioDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = TestScenario
+
+    def get_success_url(self):
+        return reverse_lazy('project-details', kwargs={'pk': self.object.project_id})
+
+    def test_func(self):
+        """
+        Check if current user is owner of the project
+        :return: Boolean
+        """
+        test_scenario = self.get_object()
+
+        if test_scenario.owner == self.request.user:  # Check if current user is owner
+            return True
+
+        return False
